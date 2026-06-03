5925b299fa9685d831f4a7b50360a5b51f0e749b
Nabil <nabilalifazmi@gmail.com>
2026-06-03 13:05:37 +0800

    feature: add input logic

--- Post_Cardiac.asm
+++ Post_Cardiac.asm
@@ -6,23 +6,80 @@
 
 .data
 welcome_msg:  .asciiz "Welcome! This program helps simulate a IOT Post-Cardiac Surgery Home Recovery Console"
-prompt_pressure_sensor:   .asciiz "Input a pressure rate (make it logical): "
 prompt_oximeter:   .asciiz "Input oxygen level (make it logical): "
-prompt_heartrate:   .asciiz "Input your current heart rate (make it logical): "
-prompt_temperature:   .asciiz "Input your current temperature (make it logical): "
+prompt_heartrate:   .asciiz "Input heart rate (make it logical): "
+prompt_pressure:   .asciiz "Input a blood pressure rate (make it logical): "
+prompt_temperature:   .asciiz "Input temperature (make it logical): "
 invalid_msg:  .asciiz "Invalid input! Please enter the correct input.\n"
 newline:      .asciiz "\n"
+
+input_buf: .space 10
 
 .text
 .globl main
 main:
-	jal  PrintMessage		# Jump to PrintMessage
+	jal PrintMessage		
 	
-	li $v0, 10			#To stop the program
+	jal PulseAndHRSensor         
+	
+	jal PressureSensor		
+	
+	jal Thermometer
+	
+	li $v0, 10			
 	syscall
+	
     
 PrintMessage:
-	li   $v0, 4			# System call code 4 = Print String
-	la   $a0, welcome_msg	# Load address of welcome message into register $a0
+	li $v0, 4			
+	la $a0, welcome_msg	
 	syscall
-    	jr   $ra
+	
+    	jr $ra
+    	
+PulseAndHRSensor:
+	# Get Oximeter
+	li $v0, 4			
+	la $a0, prompt_oximeter
+	syscall
+	
+	jal GetInput
+	
+	mov.s $f2, $f0
+	
+	# Get Heart Rate
+	li $v0, 4			
+	la $a0, prompt_heartrate
+	syscall
+	
+	jal GetInput
+	
+	mov.s $f3, $f0
+	
+	la $t1, input_buf
+	
+	# Print Oximeter
+	mov.s $f12, $f2
+	jal TestPrint
+	
+	# Print Heart Rate
+	mov.s $f12, $f3
+	jal TestPrint
+	
+    	jr $ra
+
+PressureSensor:
+
+Thermometer:
+
+GetInput:
+	li $v0, 6
+	syscall
+	
+	jr $ra
+	
+TestPrint:
+	li $v0, 2
+	syscall
+	
+	jr $ra
